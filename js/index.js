4648b1b2c311521e2bb05ae095a124e7f42dda44
Ed Ekanem <edianaekanem@gmail.com>
2021-10-16 05:27:13 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// Services Session
+
+$(function () {
+  new W0W().init();
+});
+
+// Services Session
+
+// Partners Session
+// partners carousel 
+
+$(function () {
+  $(".partners-list").owlCarousel({
+    items: 7,
+    autoplay: true,
+    smartSpeed: 700,
+    loops: true,
+    autoplayHoverPause: true,
+  });
+});
+
+// Partners Session
+
+
+
+
+// Testimonial Session
+
+$(function () {
+  $("#customers-testimonials").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 700,
+    loops: true,
+    autoplayHoverPause: true,
+  });
+});
+
+addEventListener("c");
